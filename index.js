0fe723f2f55ce275c6b7c9666b489e577b3b90b3
Nadim Absi <absinad1@gmail.com>
2022-11-23 11:41:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const submitData = (UserName,UserEmail)=>{
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: UserName,
+            email: UserEmail,
+        }),
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+        const id = object.id
+        document.querySelector("body").append(id)
+    })
+    .catch(function (error) {
+        const errorMesaage = error.message;
+        document.querySelector("body").append(errorMesaage)
+      });
+}
+
+// Add your code here
 // Add your code here
